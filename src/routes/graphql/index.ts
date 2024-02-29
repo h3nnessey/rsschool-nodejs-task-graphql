53149dc42429da466919eb327e4b05f2e64e5430
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-29 12:08:36 +0500

    docs: commit to being able to open PR

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
-
+//...
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
     url: '/',
