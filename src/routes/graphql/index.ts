a391151a3d5a682cdbed0e943bf60035a6749676
Vitaly <vitaliy.zayakin@gmail.com>
2024-03-01 14:42:34 +0500

    feat: add initial gql setup to being able to fetch users

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,69 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import { FastifyInstance } from 'fastify';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
-//...
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLNonNull,
+  parse,
+  validate,
+} from 'graphql';
+import { UUIDType } from './types/uuid.js';
+
+const userType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(UUIDType),
+    },
+    name: {
+      type: GraphQLString,
+    },
+    balance: {
+      type: GraphQLFloat,
+    },
+  }),
+});
+
+// `
+// query {
+//   users {
+//     id
+//     name
+//     balance
+//   }
+// }
+// `
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      users: {
+        type: new GraphQLList(userType),
+        resolve: async (_source, _args, { prisma }: FastifyInstance) => {
+          return await prisma.user.findMany();
+        },
+      },
+    },
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+      users: {
+        type: new GraphQLList(userType),
+        resolve: async (_source, _args, { prisma }: FastifyInstance) => {
+          return await prisma.user.findMany();
+        },
+      },
+    },
+  }),
+});
+
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
     url: '/',
@@ -12,8 +74,20 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler({ body: { query, variables } }) {
+      const document = parse(query);
+      const gqlErrors = validate(schema, document);
+
+      if (gqlErrors.length > 0) {
+        return { data: null, errors: gqlErrors };
+      }
+
+      return await graphql({
+        schema,
+        source: query,
+        variableValues: variables,
+        contextValue: fastify,
+      });
     },
   });
 };
